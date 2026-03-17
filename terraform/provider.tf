a5a67b70c404f65a390bbca225ed03173a8fa65f
Aniruddha Choudhary <choudharyaniruddhacert@gmail.com>
2026-03-17 16:59:03 +0530

    CREATE S3 BUCKET

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend s3 {
     bucket = "terraform-backend-bucket-personal-website"
-    region = "us-east-1"
+    region = "eu-north-1"
     key = "github-actions/terraform.tfstate"
     encrypt = true
     dynamodb_table = "dynamodb-terraform-pw"
